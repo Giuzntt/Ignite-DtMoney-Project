de1b181093196c939fb77b4607deafca29a01cd2
Giulianno Zanetti <giuzntt@gmail.com>
2022-04-25 19:26:47 -0300

    Atualizando Global Styles

--- src/styles/styles.ts
+++ src/styles/styles.ts
@@ -30,6 +30,14 @@ export const GlobalStyle = createGlobalStyle`
         -webkit-font-smmothing: antialiased;
 
     }
+    body, input, button, textarea{
+        font-family: 'Poppins', sans-serif;
+        font-weight: 400;
+    }
+    h1, h2, h3, h4, h5, h6, strong{
+        font-weight: 600;
+    }
+     
     button{
         cursor: pointer;
     }
